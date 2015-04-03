2525bc534330dc22f3202ae64474ede8202cf0a4
Mikael Söderström <mikael.soderstrom@live.se>
2015-04-03 13:15:20 +0200

    Initial commit

---
+++ Controllers/LedsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Raspberry.IO.GeneralPurpose;
+using System.Threading;
+
+namespace rpi_aspnet_leds.Controllers
+{
+    [Route("api/[controller]")]
+    public class LedsController : Controller
+    {
+        private static Dictionary<ushort, OutputPinConfiguration> pins;
+        private GpioConnection connection;
+
+        public LedsController()
+        {
+            pins = new Dictionary<ushort, OutputPinConfiguration> {
+                { 23, ProcessorPin.Pin23.Output() },
+                { 24, ProcessorPin.Pin24.Output() }
+            };
+
+            connection = new GpioConnection(pins[23], pins[24]);
+        }
+
+        // POST api/leds/23
+        [HttpPost("{id}")]
+        public void Put(ushort id)
+        {
+            connection.Blink(pins[id], 500);
+            Thread.Sleep(500);
+            connection.Blink(pins[id], 500);
+        }
+    }
+}
